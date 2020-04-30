02731c9b9e328b3e9f2051cc1c7d3f6a66b28986
OttPeterR <ott.peter.r@gmail.com>
2020-04-30 04:57:04 +0100

    reverting a lot of stuff

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
@@ -23,89 +21,80 @@ from mycroft import intent_file_handler
 from pixel_ring import pixel_ring
 from gpiozero import LED
 
-class LEDRingRespeaker(MycroftSkill):
+class ReSpeaker_4mic_hat(MycroftSkill):
 
-	def __init__(self):
-		super(LEDRingRespeaker, self).__init__(name="LED Ring - Respeaker")
-		self.pixel_ring = None
+    def __init__(self):
+        super(ReSpeaker_4mic_hat, self).__init__(name="ReSpeaker_4mic_hat")
 
-	def initialize(self):
-		self.log.info("Pixel Ring: Initializing")
-		self.power = LED(5)
-		self.power.on()
-		self.pixel_ring = pixel_ring
-		self.pixel_ring.set_brightness(10)
-		pattern = self.settings.get("theme", "mytheme1")
-		self.log.info(f"Input pattern: {pattern}")
-		self.pixel_ring.change_pattern(pattern)
-		self.log.info(f"Set Pattern: {self.pixel_ring.pattern}")
-		self.pixel_ring.wakeup()
-		self.enable()
+    def initialize(self):
+        self.log.info("Pixel Ring: Initializing")
+        self.power = LED(5)
+        self.power.on()
+        pixel_ring.set_brightness(10)
+	pattern = self.settings.get("theme", "mytheme1")
+	pixel_ring.change_pattern(pattern)
+        pixel_ring.wakeup()
+        self.enable()
 
-	def enable(self):
-		self.log.info("Pixel Ring: Enabling")
+    def enable(self):
+        self.log.info("Pixel Ring: Enabling")
 
-		self.add_event('recognizer_loop:wakeword',
-				self.handle_listener_wakeup)
-		self.add_event('recognizer_loop:record_end',
-				self.handle_listener_off)
+        self.add_event('recognizer_loop:wakeword',
+                self.handle_listener_wakeup)
+        self.add_event('recognizer_loop:record_end',
+                self.handle_listener_off)
 
-		self.add_event('mycroft.skill.handler.start',
-				self.handle_listener_think)
-		self.add_event('mycroft.skill.handler.complete',
-				self.handle_listener_off)
+        self.add_event('mycroft.skill.handler.start',
+                self.handle_listener_think)
+        self.add_event('mycroft.skill.handler.complete',
+                self.handle_listener_off)
 
-		self.add_event('recognizer_loop:audio_output_start',
-				self.handler_listener_speak)
-		self.add_event('recognizer_loop:audio_output_end',
-				self.handle_listener_off)
-		self.pixel_ring.off()
+        self.add_event('recognizer_loop:audio_output_start',
+                self.handler_listener_speak)
+        self.add_event('recognizer_loop:audio_output_end',
+                self.handle_listener_off)
 
-	def disable(self):
-		self.log.info("Pixel Ring: Disabling")
-		self.remove_event('recognizer_loop:wakeup')
-		self.remove_event('recognizer_loop:record_end')
-		self.remove_event('recognizer_loop:audio_output_start')
-		self.remove_event('recognizer_loop:audio_output_end')
-		self.remove_event('mycroft.skill.handler.start')
-		self.remove_event('mycroft.skill.handler.complete')
+        pixel_ring.off()
 
-	def shutdown(self):
-		self.log.info("Pixel Ring: Shutdown")
-		self.pixel_ring.off()
-		self.power.off()
+    def disable(self):
+        self.log.info("Pixel Ring: Disabling")
+        self.remove_event('recognizer_loop:wakeup')
+        self.remove_event('recognizer_loop:record_end')
+        self.remove_event('recognizer_loop:audio_output_start')
+        self.remove_event('recognizer_loop:audio_output_end')
+        self.remove_event('mycroft.skill.handler.start')
+        self.remove_event('mycroft.skill.handler.complete')
 
-	def handle_listener_wakeup(self, message):
-		pattern = self.settings.get("theme", "mytheme1")
-		self.pixel_ring.change_pattern(pattern)
-		self.log.info(f"Pixel Ring: Wake Up - Pattern Input: {pattern} - Pattern: {self.pixel_ring.pattern}")
-		self.pixel_ring.listen()
+    def shutdown(self):
+        self.log.info("Pixel Ring: Shutdown")
+        pixel_ring.off()
+        self.power.off()
 
-	def handle_listener_off(self, message):
-		self.log.info("Pixel Ring: Off")
-		self.pixel_ring.off()
+    def handle_listener_wakeup(self, message):
+        self.log.info("Pixel Ring: Wakeup")
+        pixel_ring.listen()
 
-	def handle_listener_think(self, message):
-		pattern = self.settings.get("theme", "mytheme1")
-		self.pixel_ring.change_pattern(pattern)
-		self.log.info(f"Pixel Ring: Think - Pattern Input: {pattern} - Pattern: {self.pixel_ring.pattern}")
-		self.pixel_ring.think()
+    def handle_listener_off(self, message):
+        self.log.info("Pixel Ring: Off")
+        pixel_ring.off()
 
-	def handler_listener_speak(self, message):
-		pattern = self.settings.get("theme", "mytheme1")
-		self.pixel_ring.change_pattern(pattern)
-		self.log.info(f"Pixel Ring: Speak - Pattern Input: {pattern} - Pattern: {self.pixel_ring.pattern}")
-		self.pixel_ring.speak()
+    def handle_listener_think(self, message):
+        self.log.info("Pixel Ring: Think")
+        pixel_ring.think()
 
-	@intent_handler(IntentBuilder("").require("EnablePixelRing"))
-	def handle_enable_pixel_ring_intent(self, message):
-		self.enable()
-		self.speak_dialog("EnablePixelRing")
+    def handler_listener_speak(self, message):
+        self.log.info("Pixel Ring: Speak")
+        pixel_ring.speak()
 
-	@intent_handler(IntentBuilder("").require("DisablePixelRing"))
-	def handle_disable_pixel_ring_intent(self, message):
-		self.disable()
-		self.speak_dialog("DisablePixelRing")
+    @intent_handler(IntentBuilder("").require("EnablePixelRing"))
+    def handle_enable_pixel_ring_intent(self, message):
+        self.enable()
+        self.speak_dialog("EnablePixelRing")
+
+    @intent_handler(IntentBuilder("").require("DisablePixelRing"))
+    def handle_disable_pixel_ring_intent(self, message):
+        self.disable()
+        self.speak_dialog("DisablePixelRing")
 
 def create_skill():
-	return LEDRingRespeaker()
+    return ReSpeaker_4mic_hat()
